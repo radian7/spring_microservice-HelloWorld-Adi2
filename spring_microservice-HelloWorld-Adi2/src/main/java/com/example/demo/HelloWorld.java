2ab6e7b3c0550409537d19ca889eb02468881309
radian7 <radian7@poczta.onet.pl>
2019-05-02 17:07:54 +0200

    Dodanie actuator.

--- spring_microservice-HelloWorld-Adi2/src/main/java/com/example/demo/HelloWorld.java
+++ spring_microservice-HelloWorld-Adi2/src/main/java/com/example/demo/HelloWorld.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * dla Actuator 2.0 endpoint do /actuator/health
+ * 
+ */
+
 @RestController // rodzaj beana (typu klasy zarządzanej przez Spring)
 @RequestMapping("/api")
 public class HelloWorld {
